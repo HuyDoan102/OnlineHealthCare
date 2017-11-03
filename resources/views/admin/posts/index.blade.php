ee46357d73b46baa28c6b14c385e17b3076dd95e
Huy, Nguyen Hong Doan <donghogiacat@yahoo.com.vn>
2017-11-03 09:00:30 +0700

    + show post in postscontroller

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,24 +1,68 @@
 @extends('layouts.partials.admin')
 
 @section('admin')
-<div class="container">
-    <table class="table table-striped">
-      <thead>
-        <tr>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($posts as $index => $post)
-        <tr>
-          <th scope="row">{{ $index + 1 }}</th>
-          <td><a href="{{ route("posts.show", $post->id) }}">{{ $post->title }}</a></td>
-          <td>{{ $post->content }}</td>
-          <td>{{ $post->source }}</td>
-          <td>{{ $post->created_at->format('d/m/Y') }}</td>
-      </tr>
-      @endforeach
-  </tbody>
-</table>
+<div class="content-wrapper">
+    <div class="row mb-2">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-block">
+                    <h5 class="card-title mb-4">
+                        <span>Posts Management</span>
+                        <a href="#" data-toggle="modal" data-target="#modalCreate"
+                        class="btn btn-primary"><i class="fa fa-plus-circle"></i></a>
+                    </h5>
+                    <div class="table-responsive">
+                        <table class="table center-aligned-table">
+                            <thead>
+                                <tr class="text-primary">
+                                    <th>#</th>
+                                    <th>Image</th>
+                                    <th>Title</th>
+                                    <th>Content</th>
+                                    <th>Source</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($posts as $index => $post)
+                                <tr>
+                                    <th scope="row">{{ $index + 1 }}</th>
+                                    <td><img class="image-post" src="/images/{{ $post->image }}" alt=""></td>
+                                    <td>{{ $post->title }}</td>
+                                    <td>{{ $post->content }}</td>
+                                    <td>{{ $post->source }}</td>
+                                    <td>
+                                        {{ $post->status == 0 ? "not approval" : "approval" }}
+                                    </td>
+                                    <th>
+                                        <a href="#" class="btn btn-primary btn-sm"
+                                            data-toggle="modal" data-target="#modalShow" data-id="{{ $post->id }}"
+                                            data-title="{{ $post->title }}" data-content="{{ $post->content }}"
+                                            data-source="{{ $post->source }}" data-created_at="{{ $post->created_at }}"
+                                            data-status="{{ $post->status }}" data-view="{{ $post->view }}">
+                                            <i class="fa fa-eye"></i>
+                                        </a>
+                                        <a href="#" class="btn btn-primary btn-sm"
+                                        data-toggle="modal" data-target="#modalEdit"
+                                        data-id="{{ $post->id }}" data-name="{{ $post->title }}">
+                                        <i class="fa fa-pencil-square-o"></i>
+                                        </a>
+                                        <a href="#" class="btn btn-danger btn-sm"
+                                            data-toggle="modal" data-target="#modalDelete"
+                                            data-id="{{ $post->id }}">
+                                            <i class="fa fa-trash-o"></i>
+                                        </a>
+                                    </th>
+                                </tr>
+                                @endforeach
+                                @include("admin.posts.show")
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,14 +1,83 @@
-@extends("layouts.app")
+<div class="modal fade" id="modalShow" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <!-- Modal content-->
+                <h2 class="title-modal">Post</h2>
+                <form action="" method="post">
+                    <input type="hidden" class="form-control" id="id" name="id">
+                    <div class="modal-body">
+                        <div class="form-group row">
+                            <label for="title" class="col-sm-2 col-form-label">Title</label>
+                            <div class="col-sm-10">
+                                <input class="form-control" id="title" name="title" disabled></input>
+                            </div>
+                        </div>
 
-@section("content")
+                        <div class="form-group row">
+                            <label for="title" class="col-sm-2 col-form-label">Content</label>
+                            <div class="col-sm-10">
+                                <input class="form-control" id="content" name="content" disabled></input>
+                            </div>
+                        </div>
 
-<div class="container">
+                        <div class="form-group row">
+                            <label for="title" class="col-sm-2 col-form-label">Source</label>
+                            <div class="col-sm-10">
+                                <input class="form-control" id="source" name="source" disabled></input>
+                            </div>
+                        </div>
 
-  <a href="{{ route("posts.index") }}">&#x21A9;Back Home Post</a>
-  <h2>POST #{{$post->id}}</h2>
-  <p>{{ $post->title }}</p>
-  <p>{{ $post->content }}</p>
+                        <div class="form-group row">
+                            <label for="title" class="col-sm-2 col-form-label">Time</label>
+                            <div class="col-sm-10">
+                                <input class="form-control" id="created_at" name="created_at" disabled></input>
+                            </div>
+                        </div>
 
+                        <div class="form-group row">
+                            <label for="title" class="col-sm-2 col-form-label">Status</label>
+                            <div class="col-sm-10">
+                                <input class="form-control" id="status" name="status" disabled></input>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="title" class="col-sm-2 col-form-label">View</label>
+                            <div class="col-sm-10">
+                                <input class="form-control" id="view" name="view" disabled></input>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 
-@endsection
+<script>
+  $('#modalShow').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var id = button.data('id');
+  var title = button.data('title');
+  var content = button.data('content');
+  var source = button.data('source');
+  var created_at = button.data('created_at');
+  var status = button.data('status');
+  var view = button.data('view');
+
+  $('#id').val(id);
+  $('#title').val(title);
+  $('#content').val(content);
+  $('#source').val(source);
+  $('#created_at').val(created_at);
+  $('#status').val(status);
+  $('#view').val(view);
+})
+</script>
+
+
+
