be9280d1f2cc00e09156634cc80ef89386fb51a9
Huy, Nguyen Hong Doan <donghogiacat@yahoo.com.vn>
2017-12-03 09:16:05 +0700

    @ edit relationship table article and user

---
+++ database/migrations/2017_12_03_014558_add_column_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropColumn('email');
+            $table->string('creator');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+        });
+    }
+}
