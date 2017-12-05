334144a69df89ee70b49636f24a7afef9bad8994
John Doe <trantruongquy2702@gmail.com>
2017-12-05 21:06:06 +0700

    + related Post

--- app/Post.php
+++ app/Post.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 use App\TypeOfDisease;
+use App\DetailPost;
 
 class Post extends Model
 {
@@ -18,5 +19,15 @@ class Post extends Model
     public function type_of_diseases()
     {
         return $this->belongsToMany(TypeOfDisease::class, 'detail_posts', 'post_id', 'type_of_diseases_id');
+    }
+
+    public function relatedPosts()
+    {
+    	$type_ids = $this->type_of_diseases->pluck('id')->toArray();
+    	$post_ids = DetailPost::whereIn('type_of_diseases_id', $type_ids)
+    		->where('post_id', '<>', $this->id)
+    		->pluck('post_id')
+    		->toArray();
+    	return Post::whereIn('id', $post_ids)->limit(6)->orderBy('created_at', 'desc')->get();
     }
 }
